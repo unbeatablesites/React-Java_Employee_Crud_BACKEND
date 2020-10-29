db417ed462cb4e7ed324e441e36941dca4faa9a8
Frank Uzoka <Frankuzoka@hotmail.com>
2020-10-29 00:43:16 -0400

    Fix cross origin

--- src/main/java/com/crudEmployee/crudEmployee/controller/EmployeeController.java
+++ src/main/java/com/crudEmployee/crudEmployee/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://crud-employee-jave-fe.herokuapp.com")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
